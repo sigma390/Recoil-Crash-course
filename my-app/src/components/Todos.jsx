bce65183084590f503e5c9b2e93e7296d33c915d
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-22 13:55:23 +0530

    Wrapped in Recoil ROOT

---
+++ my-app/src/components/Todos.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <main>
+        <input type="text" id='todoInpt' />
+        <button>Add</button>
+        <button>Delete</button>
+
+
+
+    </main>
+  )
+}
+
+export default Todos;
